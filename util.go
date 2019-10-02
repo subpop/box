57cb79a1800a764bfda0a34a7ba2af0954929e0f
Link Dupont <link@sub-pop.net>
2019-10-02 12:58:42 -0400

    add utils

---
+++ util.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func getDataDir() (string, error) {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	dir = filepath.Join(dir, ".local", "share", "box")
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return "", err
+		}
+	}
+
+	return dir, nil
+}
+
+func getImagesDir() (string, error) {
+	dir, err := getDataDir()
+	if err != nil {
+		return "", err
+	}
+
+	dir = filepath.Join(dir, "images")
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return "", err
+		}
+	}
+
+	return dir, nil
+}
