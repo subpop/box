53fc7f889e8985891442b03115d179925e4f362a
Link Dupont <link@sub-pop.net>
2019-11-01 12:28:24 -0400

    Improve Down operation
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- down.go
+++ down.go
@@ -2,44 +2,42 @@ package vm
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/libvirt/libvirt-go"
 )
 
 // Down looks up an active domain by name and stops it. If force is true, the
-// domain is stopped ungracefully.
-func Down(name string, id int, force bool) error {
+// domain is stopped without prompting for confirmation. If graceful is true,
+// the domain is shutdown gracefully.
+func Down(name string, force bool, graceful bool) error {
 	conn, err := libvirt.NewConnect("")
 	if err != nil {
 		return err
 	}
 
-	if len(name) > 0 && id > 0 {
-		return fmt.Errorf("conflicting arguments: name, id")
-	}
-
-	var dom *libvirt.Domain
-	if len(name) > 0 {
-		dom, err = conn.LookupDomainByName(name)
-		if err != nil {
-			return nil
-		}
-	} else if id > 0 {
-		dom, err = conn.LookupDomainById(uint32(id))
-		if err != nil {
-			return nil
-		}
-	} else {
-		return fmt.Errorf("conflicting arguments: name, id")
+	dom, err := conn.LookupDomainByName(name)
+	if err != nil {
+		return nil
 	}
 	defer dom.Free()
 
-	if force {
-		if err := dom.Destroy(); err != nil {
+	if !force {
+		fmt.Printf("Are you sure you wish to stop %v? (y/N) ", name)
+		var response string
+		fmt.Scan(&response)
+		response = strings.ToLower(strings.TrimSpace(response))
+		if response != "y" {
+			return nil
+		}
+	}
+
+	if graceful {
+		if err := dom.Shutdown(); err != nil {
 			return err
 		}
 	} else {
-		if err := dom.Shutdown(); err != nil {
+		if err := dom.Destroy(); err != nil {
 			return err
 		}
 	}
